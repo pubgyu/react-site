640d80a8ea7f56abaa95d967620210c7a00e0daa
pub <63778651+pubgyu@users.noreply.github.com>
2021-11-30 10:08:34 +0900

    story

--- src/routes/Redux.js
+++ src/routes/Redux.js
@@ -2,13 +2,17 @@ import { useState } from "react";
 import {createStore} from 'redux';
 
 function Redux() {
+    const [todo,setTodo] = useState([]);
+    // const [value,setValue] = useState('');
+
     const ADD_TODO = 'ADD_TODO';
     const DELETE_TODO = 'DELETE_TODO';
-
-    const reducer = (state = [] ,action) => {
+    const initState = [];
+    
+    const reducer = (state = initState ,action) => {
         switch(action.type) {
             case ADD_TODO : 
-                return state = [value];
+            return [...state, {id: action.id, arr: action.newItem}]
 
             case DELETE_TODO : 
                 return state = [];
@@ -19,31 +23,47 @@ function Redux() {
     }
     const store = createStore(reducer);
 
-    const [todo,setTodo] = useState([]);
-    const [value,setValue] = useState('');
+    store.subscribe(()=>console.log('data : ', store.getState()));
+    store.subscribe(()=>paintsToDos());
 
-    const createTodo = t => {
-        setTodo([t, ...todo]);
+    const paintsToDos = () => {
+        // const todos = store.getState();
+        // setTodo(todos);
+    }
+    
+    const addTodoFunc = (item,id) => {
+        store.dispatch({
+            type : ADD_TODO, 
+            newItem : item,
+            id : id
+        });
     }
     const onSubmit = e => {
         e.preventDefault();
-        // createTodo(value);
-        store.dispatch({type : ADD_TODO, text : value});
-        setValue('');
+        // store.dispatch({type : ADD_TODO, newItem : value});
+        // setValue('');
+        if(document.querySelector('input').value !== '') {
+            addTodoFunc(document.querySelector('input').value, Date.now());
+        }
+
+        document.querySelector('input').value = '';
     }
     const onChange = e => {
-        setValue(e.target.value);
+        // setValue(e.target.value);
     }
     return (
         <>
+            <div>
+                <h2>Reduce State : {}</h2>
+            </div>
             <form onSubmit={onSubmit}>
-                <input type="text" value={value} onChange={onChange} placeholder="todo"/>
+                <input type="text" onChange={onChange} placeholder="todo"/>
                 <button>add</button>
             </form>
             <ul>
-                {todo.map((i,idx)=>{
-                    return <li key={idx}>{i}</li>
-                })}
+                {/* {todo.map((i)=>{
+                    return <li key={i}>{i}</li>
+                })} */}
             </ul>
         </>
     )
